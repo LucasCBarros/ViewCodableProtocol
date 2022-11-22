958b8eb2163394cb026d419777a0b14e22fb47dd
Lucas C. Barros <lucas_cb@icloud.com>
2022-11-21 23:10:54 -0800

    Create ViewCodable.swift
    Adding protocol file

---
+++ ViewCodable.swift
@@ -0,0 +1,84 @@
+//
+//  ViewCodable.swift
+//  BattleScape
+//
+//  Created by Lucas C Barros on 2022-10-16.
+//  Copyright © 2022 Lucas C Barros. All rights reserved.
+//
+
+import UIKit
+
+//MARK: - Protocol
+/*
+    This is a protocol I use to easily organize UI handling code.
+    Simply adopt this protocol, customize the methods which suit you best and call the main setup function.
+*/
+protocol ViewCodable {
+    func setupUI()
+    func addHierarchy()
+    func addConstraints()
+    func additionalConfig()
+}
+
+extension ViewCodable {
+    func setupUI() {
+        addHierarchy()
+        addConstraints()
+        additionalConfig()
+    }
+}
+
+protocol ViewCodable {
+    
+    /*
+        This function calls all other functions in the correct order.
+        You can use it in an UIViewController viewDidLoad method or in a view initializer, for example.
+    */
+    func setup()
+    
+    //  This function should be used to add your custom views to the views hierarchy in correct order
+    func addToHierarchy()
+    
+    // This function should be used to add constraints to your customs views
+    func setupConstraints()
+    
+    /*
+        This function should be used to apply styles to your customs views.
+        You could change colors, fonts, alignments or other properties here.
+    */
+    func setupStyles()
+    
+    /*
+        This function should be used to link actions to your customs views.
+        For example, you could add a selector to a button or use reactive bindings here.
+    */
+    func bindComponents()
+    
+    /*
+        This function should be used to assign accessibility idientifiers to your custom views.
+        It might come in handy if you are writing UI tests.
+    */
+    func setupAcessibilityIdentifiers()
+}
+
+
+//MARK: - Default implementations
+extension ViewCodable {
+    
+    func setup() {
+        addToHierarchy()
+        setupConstraints()
+        setupStyles()
+        bindComponents()
+        setupAcessibilityIdentifiers()
+    }
+    
+    // Making some of the functions optional, since they might not apply to every context
+
+    func setupStyles() { }
+    
+    func bindComponents() { }
+    
+    func setupAcessibilityIdentifiers() { }
+    
+}
